4eb3a453b4a0fbcdb70ba639b2d6a88d86b45467
Ahmed <tauqeer.ahmed@wissen.com>
2023-08-15 19:15:59 +0530

    Initial commit

---
+++ src/com/dsa/arrays/operations/Operations.java
@@ -0,0 +1,32 @@
+package com.dsa.arrays.operations;
+
+public class Operations {
+
+    public static void main(String[] args) {
+
+    initialization();
+
+    }
+
+    public static void initialization() {
+        /*
+         *  Declaration
+         *  Syntax : dataType[] arrayName;
+         *
+         *  Initialization
+         *  arrayName = new int[10];
+         **/
+
+        int[] myArray;
+        myArray = new int[10];
+
+        int[] myArrayTwo  = new int[10];
+
+        int[] myArrayThree = {1,2,3};
+
+
+    }
+
+
+    
+}
